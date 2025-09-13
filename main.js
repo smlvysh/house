0758fc618610cf4ef1c6bead793322a2954266d1
IsmailovaAisha <oyshaismailova08@gmail.com>
2025-09-14 00:44:37 +0500

    kospekt+misol

---
+++ main.js
@@ -0,0 +1,51 @@
+result= " "
+for( let i=1; i<140; i++){
+    if(i % 2 === 0)
+    result +=i+ " ";
+
+}console.log(result);
+
+
+
+otvet=" " 
+for(let i=100; i<240; i++){
+    if(i % 2 !==0)
+        otvet +=i+" ";
+   
+} console.log(otvet);
+
+
+number= " "
+for(i=40; i<180; i++){
+    if(i % 7 ===0)
+        number +=i +" ";
+   
+} console.log(number);
+
+
+result1=" "
+for(i=50; i<140; i++){
+    if(i%11 === 0)
+        result1+=i +" "
+}console.log(result1);
+
+
+otvet1=" "
+for(i=90; i<210; i++){
+    if(i% 25 ===0)
+        otvet1+=" "
+}console.log(otvet1);
+
+number1=" "
+for(i=25; i<690; i++ ){
+    if(i% 5 ===0 && i%9===0)
+        number1+=i +" "
+}console.log(number1)
+
+
+
+abc=" "
+for(i=1; i<4000; i++){
+    if(i%3===0 && i%9===0 && i%11===0)
+        abc+=i+" "
+} console.log(abc);
